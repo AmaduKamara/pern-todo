777a4dd0abebd15ce4148c4128b029b28a337157
Amkam <amaduskamara@gmail.com>
2022-06-17 23:59:10 +0000

    Add middlewares for cors and req body

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 // Init express app
 const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 app.listeners(5000, () => {
   console.log("Server running on port: 5000");
