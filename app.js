ce2a364268c5828bdab7071677dec2c38936d2fa
Amkam <amaduskamara@gmail.com>
2022-06-18 00:33:43 +0000

    Create database connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 // Init express app
 const app = express();
@@ -8,6 +9,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listeners(5000, () => {
+// Routes
+
+// Create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todos (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.status(201).json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// GET all todos
+
+// GET a todo
+
+// Update a todo
+
+// DELETE a todo
+
+app.listen(5000, () => {
   console.log("Server running on port: 5000");
 });
